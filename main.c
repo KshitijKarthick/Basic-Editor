63b35f6889c0be5e374c5b5a551a5ecc17efe05e
Kshitij Karthick <kshitij.karthick@gmail.com>
2014-10-24 13:02:04 +0530

    OS Independent ClearScreen Functionality and Comments Reformatting

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 /*
- * File:   main.c
- * Author: kshitijkarthick
- *
+ * File:   	main.c
+ * Author: 	kshitijkarthick
+ * Program:	Basic Editor
  * Created on August 31, 2014, 4:49 PM
- */
+*/
 
 #include<stdio.h>
 #include<stdlib.h>
 #include "conio.h"
 
-struct node																													// Double Linked List Implementation.
+// Double Linked List Implementation.
+struct node
 {
 	char info[10];
 	struct node *llink;
@@ -17,8 +18,8 @@ struct node																													// Double Linked List Implementation.
 };
 
 typedef struct node *Node;
-Node root = NULL;																										// Root node creation
-char* FILENAME;																											// Filename given as cmd line arguments
+Node root = NULL;  // Root node creation.
+char* FILENAME;    // Filename given as cmd line arguments.
 
 Node createNode(Node);
 Node deleteNode(Node);
@@ -26,6 +27,8 @@ void print();
 void save();
 void input();
 void lines(int);
+void clearscr();
+
 /*
 	Receive the command line argument for filename and call the input function
 */
@@ -34,7 +37,7 @@ int main(int argc,char* argv[])
 	root=(Node) malloc(sizeof(struct node));
 	if(argc>1)
 	{
-		FILENAME=argv[1];																								// Filename stored for creating and storing data.
+		FILENAME=argv[1]; // Filename stored for creating and storing data.
 		input();
 	}
 	else
@@ -47,7 +50,7 @@ int main(int argc,char* argv[])
 */
 Node createNode(Node parent)
 {
-	Node temp=(Node) malloc(sizeof(struct node));											// Node Creation and joining the Node with previous nodes.
+	Node temp=(Node) malloc(sizeof(struct node)); // Node Creation and joining the Node with previous nodes.
 	parent->rlink=temp;
 	temp->llink=parent;
 	return temp;
@@ -59,13 +62,17 @@ Node createNode(Node parent)
 Node deleteNode(Node current)
 {
 	Node temp=current->llink;
-	if(current!=root)																										// Delete a Node if it is not the root node.
+
+	// Delete a Node if it is not the root node.
+	if(current!=root)
 	{
 		free(current);
 		temp->rlink=NULL;
 		return temp;
 	}
-	else																																// Warning message if explicit Root node deletion.
+
+	// Warning message if explicit Root node deletion.
+	else
 	{
 		printf("\n\nError! cannot delete root node \nPress Enter key to continue");
 		getchar();
@@ -81,12 +88,14 @@ void save()
 {
 	FILE *fp=fopen(FILENAME,"w");
 	Node temp=root;
-	while(temp!=NULL)																									// Node traversal from the root node to all other nodes.
+
+	// Node traversal from the root node to all other nodes.
+	while(temp!=NULL)
 	{
-		fprintf(fp,temp->info);																					// Store data of each node in the File.
+		fprintf(fp,temp->info); // Store data of each node in the File.
 		temp=temp->rlink;
 	}
-	fclose(fp);
+	fclose(fp); // Close File Stream.
 	printf("\n\t\t\tFile Saved Successfully\n");
 }
 
@@ -96,8 +105,7 @@ void save()
 void print()
 {
 	int i;
-	//system("cls"); 																									// For dos operating system.
-	system("clear");																									// For unix operating system.
+	clearscr();
 	lines(2);
 	printf("\n\t\t\tBasic Editor\n");
 	printf("\t\t\tSave Option : ctrl + c or ctrl + z\n");
@@ -105,10 +113,12 @@ void print()
 	lines(0);
 	printf("\t\t\tInput Field:\n\n");
 	Node temp=root;
-	while(temp!=NULL)																									// Node traversal from the root node to all other nodes.
+
+	// Node traversal from the root node to all other nodes.
+	while(temp!=NULL)
 	{
 		for(i=0;i<10;i++)
-			printf("%c",temp->info[i]);																		// Print the data of each node to the screen.
+			printf("%c",temp->info[i]); // Print the data of each node to the screen.
 		temp=temp->rlink;
 	}
 }
@@ -120,55 +130,67 @@ void input()
 {
 	print();
 	char ch;
-	int noOfChar=0;																										// No of Characters in the current Node.
-	int totalNoOfLines=1;																							// Total no of NewLine Characters.
-	int totalNoOfChar=0;																							// Total no of Characters.
+	int noOfChar=0;       // No of Characters in the current Node.
+	int totalNoOfLines=1;	// Total no of NewLine Characters.
+	int totalNoOfChar=0;	// Total no of Characters.
 	Node temp=root;
 	ch=getche();
-	while( ch != 3 && ch != 26 )																			// Receive input and check if input is not ctrl + c and ctrl + z
+
+	// Receive input and check if input is not ctrl + c and ctrl + z
+	while( ch != 3 && ch != 26 )
 	{
 		noOfChar++;
 		//printf("\n%d->%c:%d\n",noOfChar,ch,ch);
-		if(( ch == 8 || ch == 127 ) && noOfChar > 1)										// Check Backspace character
+		if(( ch == 8 || ch == 127 ) && noOfChar > 1) // Check Backspace character
 		{
-			noOfChar--;																										// Remove the count of Backspace as character.
-			(temp->info[noOfChar-1]=='\n')?(totalNoOfChar--,totalNoOfLines--):(totalNoOfChar--);
-																																		// Reduce count of characters deleted.
-			if(noOfChar == 1)																							// if previous node character to be deleted then delete current node and delete previous node last character
+			noOfChar--;	// Remove the count of Backspace as character.
+			(temp->info[noOfChar-1]=='\n')?(totalNoOfChar--,totalNoOfLines--):(totalNoOfChar--); // Reduce count of characters deleted.
+
+			// if previous node character to be deleted then delete current node and delete previous node last character
+			if(noOfChar == 1)
 			{
-				if(temp!=root)																							//Normal Node Deletion.
+
+				//Normal Node Deletion.
+				if(temp!=root)
 				{
 					temp=deleteNode(temp);
 					noOfChar=10;
 				}
-				else																												// Root node not deleted, only last character made null.
+
+				// Root node not deleted, only last character made null.
+				else
 				{
 							temp->info[0]='\0';
 							noOfChar=0;
 				}
 			}
-			else if(noOfChar < 11)																				// Delete Last character and append null in that position.
+
+			// Delete Last character and append null in that position.
+			else if(noOfChar < 11)
 			{
 				temp->info[noOfChar - 1]='\0';
 				noOfChar--;
 			}
 			print();
 		}
-		else if(noOfChar > 10)																					// Node Creation for adding the Character.
+
+		// Node Creation for adding the Character.
+		else if(noOfChar > 10)
 		{
-			(ch=='\n')?(totalNoOfChar++,totalNoOfLines++):(totalNoOfChar++);
-																																		// Add Count of Character and NewLine.
+			(ch=='\n')?(totalNoOfChar++,totalNoOfLines++):(totalNoOfChar++); // Add Count of Character and NewLine.
 			temp=createNode(temp);
 			noOfChar=1;
 			temp->info[noOfChar-1]=ch;
 		}
-		else if(noOfChar < 11)																					// Normal character addition in the same Node.
+
+		// Normal character addition in the same Node.
+		else if(noOfChar < 11)
 		{
-			(ch=='\n')?(totalNoOfChar++,totalNoOfLines++):(totalNoOfChar++);
-																																		// Add Count of Character and NewLine.
+			(ch=='\n')?(totalNoOfChar++,totalNoOfLines++):(totalNoOfChar++); // Add Count of Character and NewLine.
+
 			temp->info[noOfChar - 1]=ch;
 		}
-		ch=getche();																										// Character Input from conio.h.
+		ch=getche(); // Character input from conio.h.
 	}
 	print();
 	lines(0);
@@ -177,6 +199,7 @@ void input()
 	printf("\n\t\t\tNo Of Characters:%d\n\t\t\tNo of Lines:%d\n",totalNoOfChar,totalNoOfLines);
 	lines(1);
 }
+
 /*
 	Printing the required type of Lines.
 */
@@ -193,4 +216,22 @@ void lines(int option)
 		default:
 			printf("\n------------------------------------------------------------------------\n");
 	}
+}
+
+/*
+	OS Independent clear screen functionality.
+*/
+void clearscr()
+{
+	#if defined(__CYGWIN__) && !defined(_WIN32)
+	/* Cygwin POSIX under Microsoft Windows. -------------------- */
+  	system("cls");
+	#elif !defined(_WIN32) && (defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__)))
+	/* UNIX-style OS. ------------------------------------------- */
+		system("clear");
+	#else
+		printf("\n\n\t\tSorry the OS is not supported,the editor cannot function properly in this target OS\n\t\t\tPress Enter key to Exit.");
+		getchar();
+		exit(-1);
+	#endif
 }
